8fdc6e1b7131e570adfaa322ec66ae5e358102c5
marky376 <mmutuamark@gmail.com>
2024-01-23 18:05:37 +0300

    A commit on some files to work on

--- sort.h
+++ sort.h
@@ -1,5 +1,14 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void insertion_sort_list(listint_t **list);
+void bubble_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
